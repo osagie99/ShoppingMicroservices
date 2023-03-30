87d912d6edf3114b707706eef0706a325140ea4d
Osagie <osagie@Osagies-MacBook-Pro.local>
2023-03-30 23:34:28 +0100

    Created JWT service

---
+++ AuthenticationService/src/main/java/com/surge/AuthenticationService/config/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.surge.AuthenticationService.config;
+
+public class JwtAuthenticationFileter {
+}
---
+++ AuthenticationService/src/main/java/com/surge/AuthenticationService/config/JwtService.java
@@ -0,0 +1,2 @@
+package com.surge.AuthenticationService.config;public class JwtService {
+}
