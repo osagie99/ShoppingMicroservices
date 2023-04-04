5dfb1ac700e0ded8d7d913d2e1bb8f0f7f0acd1c
Osagie <osagie@Osagies-MacBook-Pro.local>
2023-04-04 13:17:19 +0100

    Updated Auth response and add Test Case for order service

---
+++ AuthenticationService/src/main/java/com/surge/AuthenticationService/dto/BaseResponse.java
@@ -0,0 +1,18 @@
+package com.surge.OrderService.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BaseResponse<T> {
+    private String status;
+
+    private String message;
+
+    private T data;
+}
